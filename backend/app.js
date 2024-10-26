578df5f13ce32329434df8f1f037bea597c2916b
KRITHIKus <krithikus2004@gmail.com>
2024-10-26 17:12:21 +0530

    deploy

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const dotenv=require('dotenv')
+const path=require('path')
+const cors=require('cors')
+const connectDatabase=require('./config/connectdb.js')
+dotenv.config({path:path.join(__dirname,'config','config.env')});
+    
+const products = require('./routes/products.js');
+
+const order=require('./routes/order.js')
+
+connectDatabase();
+app.use(express.json())
+app.use(cors())
+app.use('/api/v1',products)
+app.use('/api/v1',order)
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+
+  
+app.listen(process.env.PORT,()=>{
+    console.log(`Server listening to port ${process.env.PORT} in ${process.env.NODE_ENV}`)
+});
